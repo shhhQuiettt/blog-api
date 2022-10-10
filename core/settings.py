9b833a5c769804fc421ac8d67fdc011847fa5478
shhhQuiettt <krzysztof.skro@gmail.com>
2022-10-10 10:58:06 +0200

    Rename main project directory

--- core/settings.py
+++ core/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "blog.urls"
+ROOT_URLCONF = "core.urls"
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "blog.wsgi.application"
+WSGI_APPLICATION = "core.wsgi.application"
 
 
 # Database
